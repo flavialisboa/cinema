171cc42649d5bdc7de78db781836bd44e06925c1
Flavia Lisboa <66700064+flavialisboa@users.noreply.github.com>
2021-05-20 08:50:36 -0300

    Atualização de busca de filmes

---
+++ js/filtra.js
@@ -0,0 +1,25 @@
+var campoFiltro = document.querySelector("#filtrar-filmes");
+
+campoFiltro.addEventListener("input", function(){
+    console.log(this.value);
+    var filmes = document.querySelectorAll(".filme-escolha");
+
+    if(this.value.length > 0){
+        for(var i = 0; i < filmes.length; i++){
+            var filme = filmes[i];
+            var pNome = filme.querySelector(".titulo-filmes");
+            var nomeFilme = pNome.textContent;
+            
+            if(nomeFilme != this.value){
+                filme.classList.add("invisivel");
+            }else{
+                filme.classList.remove("invisivel");
+            }
+        }
+    }else{
+        for(var i=0; filmes.length; i++){
+            var filme = filmes[i];
+            filme.classList.remove("invisivel");
+        }
+    }
+});
---
+++ js/script.js
@@ -0,0 +1,73 @@
+function nomeFilme(){
+    var selecao = document.querySelector("#selecaoFilmes");
+    var opcao = selecao.children[selecao.selectedIndex];
+    var filme = opcao.textContent;
+
+    return filme;
+}
+
+function valorIngressos(){
+    var entradasInteiras = document.querySelector("#numeroEntradasInteiras");
+    var quantidadeInteiras = entradasInteiras.children[entradasInteiras.selectedIndex];
+    var quantidadeEntradasInteiras = quantidadeInteiras.textContent;
+
+    var entradasMeias = document.querySelector("#numeroEntradasMeias");
+    var quantidadeMeias = entradasMeias.children[entradasMeias.selectedIndex];
+    var quantidadeEntradasMeias = quantidadeMeias.textContent;
+
+    var valorInteiras = 20;
+    var valorMeias = 10;
+
+    var valorTotal = quantidadeEntradasInteiras * valorInteiras + quantidadeEntradasMeias * valorMeias;
+
+    return Number(valorTotal);
+}
+
+function horarioEscolhido(){
+    var horario = document.querySelector('input[name="ckHorario"]:checked').value;
+
+    return horario;
+}
+
+function escolheAssento(){
+    //  var divCadeiras = document.querySelector('#div_cadeiras');
+      //divCadeiras.addEventListener('click', () => {
+        var cadeira = document.getElementsByName('ckAssento');
+          
+        assentosEscolhidos = [];
+
+        cadeira.forEach(elemento => {
+            if(elemento.checked){
+                assentosEscolhidos.push(elemento.value);
+            } 
+        });
+}
+ 
+function comprar() {
+    var filme = nomeFilme();
+    var valorTotal = valorIngressos();
+    var horario = horarioEscolhido();
+    var elem_msg = document.getElementById("div_msg");
+        
+    escolheAssento(); 
+
+    if(valorTotal == 0){
+        alert("Selecione a quantidade de ingressos!")
+    } else {
+        alert("Compra realizada!");
+        elem_msg.innerHTML = "Resumo do pedido:<br><br>Você selecionou o filme:<br>" + filme + ", no horário de " + horario + ", no valor de R$ " + valorTotal + ",00. <br>Seus assentos são:<br>" + assentosEscolhidos;
+    }
+}
+
+var assentosEscolhidos = [];
+
+/*cadeira.forEach(elemento => {
+            if(elemento.checked){
+                assentosEscolhidos.push(elemento.value);
+            } 
+        });
+    });
+
+    for (var i = 0; i < cadeira.length; i++) {
+        if (cadeira[i].checked) assentosEscolhidos.push(cadeira[i].value);
+    }*/
